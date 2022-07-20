8454e189a59b930b0695ab414cbd24f9430a6a1b
caiosantosss <caio.santos@msn.com>
2022-07-20 20:55:26 +0900

    add style on discover

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -5,6 +5,11 @@ import { useRouter } from 'next/router';
 import { topics } from '../utils/constants';
 
 const Discover = () => {
+
+  const activeTopicStyle = ''
+
+  const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray 300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black'
+
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
       <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
@@ -13,11 +18,11 @@ const Discover = () => {
       <div className='flex gap-3 flex-wrap'>
         {topics.map((item) => (
           <Link href={`/?topic=${item.name}`} key={item.name}>
-            <div>
-              <span>
+            <div className={topicStyle}>
+              <span className='font-bold text-2xl xl:text-md'>
                 {item.icon}
               </span>
-              <span>
+              <span className='font-medium text-md hidden xl:block capitalize'>
                 {item.name}
               </span>
             </div>
