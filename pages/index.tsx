7687f69e003469dbcfb9fba4334d3608d766c3f8
caiosantosss <caio.santos@msn.com>
2022-07-23 18:57:15 +0900

    add types.d.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
 import axios from 'axios';
 
+// interface IProps {
+//   videos:
+// }
+
 const Home: NextPage = ({ videos }) => {
+  console.log(videos);
   return (
     <div className='text-3x1 font-bold underline'>
       Caio Video Share
