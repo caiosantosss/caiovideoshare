883c9b45b31f04556a5d84efcc90a83334efdf08
caiosantosss <caio.santos@msn.com>
2022-07-27 19:46:34 +0900

    add function to play and pause

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -15,6 +15,17 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
   const [isHover, setIsHover] = useState(false);
   const [playing, setPlaying] = useState(false);
   const [isVideoMuted, setIsVideoMuted] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoPres = () => {
+    if (playing) {
+      videoRef?.current?.pause();
+      setPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setPlaying(true);
+    }
+  }
 
   return (
     <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
@@ -57,6 +68,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
           <Link href='/'>
             <video
               loop
+              ref={videoRef}
               className='lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
               src={post.video.asset.url}
             >
@@ -74,7 +86,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                 <button>
                   <BsFillPlayFill className='text-black text-2xl lg:text-4xl' />
                 </button>
-              )}
+              )},
               {isVideoMuted ? (
                 <button>
                   <HiVolumeOff className='text-black text-2xl lg:text-4xl' />
