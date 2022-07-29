1759b5a69e456c691ddd1c660729ba88e0a1ca14
caiosantosss <caio.santos@msn.com>
2022-07-29 23:05:11 +0900

    fix style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ import Logo from '../utils/csv-logo.png';
 
 const Navbar = () => {
   return (
-    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-3'>
       <Link href='/'>
         <div className='w-[100px] md:w-[130px]'>
           <Image
