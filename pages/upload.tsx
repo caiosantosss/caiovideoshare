8cab2c5f0164186a1903e163dda5c43fd8895416
caiosantosss <caio.santos@msn.com>
2022-08-04 21:32:54 +0900

    add update div and style

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -9,7 +9,19 @@ import { client } from '../utils/client';
 
 const Upload = () => {
   return (
-    <div>Upload</div>
+    <div className='flex w-full h-full'>
+      <div className='bg-white rounded-lg'>
+        <div>
+          <div>
+            <p className='text-2xl font-bold'>Upload Videos</p>
+            <p className='text-md text-gray-400 mt-1'>Post a video to your account</p>
+          </div>
+          <div className='border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px]'>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
