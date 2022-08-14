b8dadaf4bb19d3559a8eba06b0c46879e3f739e9
caiosantosss <caio.santos@msn.com>
2022-08-14 18:27:36 +0900

    add import

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect, useRef} from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Link from 'next/link';
+import { GoVerified } from 'react-icons/go';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import axios from 'axios';
 
 const Detail = () => {
   return (
