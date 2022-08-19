cf8ed76651c77a9590e299dc5f3f2e481892b90b
caiosantosss <caio.santos@msn.com>
2022-08-19 22:07:00 +0900

    add style

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -16,6 +16,7 @@ interface Iprops {
 
 const Detail = ({ postDetails }: Iprops ) => {
   const [post, setPost] = useState(postDetails);
+  const [playing, setPlaying] = useState(false);
 
   const videoRef = useRef(null);
 
@@ -23,7 +24,7 @@ const Detail = ({ postDetails }: Iprops ) => {
 
   return (
     <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
-      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
+      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black'>
         <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
           <p>
             < MdOutlineCancel className='text-white text-[35px]' />
@@ -32,10 +33,20 @@ const Detail = ({ postDetails }: Iprops ) => {
         <div className='relative'>
           <div className='lg:h-[100vh] h-[60vh]'>
             <video
+              ref={videoRef}
+              loop
+              onClick={onVideoClick}
               src={post.video.asset.url}
+              className='h-full cursor-pointer'
             >
-
             </video>
+          </div>
+          <div className='absolute top-[45%] left-[45%] cursor-pointer'>
+            {!playing && (
+              <button onClick={onVideoClick} >
+                <BsFillPlayFill className='text-white text-6xl lg:text-8xl' />
+              </button>
+            )}
           </div>
         </div>
       </div>
