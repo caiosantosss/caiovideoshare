d3d043c16b71fa2473c221a09ca29b231d0e8ba2
caiosantosss <caio.santos@msn.com>
2022-08-20 16:53:19 +0900

    add video muted unmuted

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -18,6 +18,8 @@ interface Iprops {
 const Detail = ({ postDetails }: Iprops ) => {
   const [post, setPost] = useState(postDetails);
   const [playing, setPlaying] = useState(false);
+  const [isVideoMuted, setisVideoMuted] = useState(false);
+
   const videoRef = useRef<HTMLVideoElement>(null);
 
   const onVideoClick = () => {
@@ -58,6 +60,17 @@ const Detail = ({ postDetails }: Iprops ) => {
               </button>
             )}
           </div>
+        </div>
+        <div className='absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer'>
+          {isVideoMuted ? (
+            <button onClick={() => setIsVideoMuted(false)}>
+              <HiVolumeOff className='text-white text-2xl lg:text-4xl' />
+            </button>
+          ) : (
+            <button onClick={() => setIsVideoMuted(true)}>
+              <HiVolumeUp className='text-white text-2xl lg:text-4xl' />
+            </button>
+          )}
         </div>
       </div>
     </div>
