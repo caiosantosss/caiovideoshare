ba958b3af9036896b3fcd419ef4c9252c43ec376
caiosantosss <caio.santos@msn.com>
2022-08-21 19:43:07 +0900

    add name and image

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -18,7 +18,7 @@ interface Iprops {
 const Detail = ({ postDetails }: Iprops ) => {
   const [post, setPost] = useState(postDetails);
   const [playing, setPlaying] = useState(false);
-  const [isVideoMuted, setisVideoMuted] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
   const router = useRouter();
 
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -78,6 +78,40 @@ const Detail = ({ postDetails }: Iprops ) => {
               <HiVolumeUp className='text-white text-2xl lg:text-4xl' />
             </button>
           )}
+        </div>
+      </div>
+
+      <div className='relative w-[1000px] md:w-[900px] lg:w-[700px]'>
+        <div className='lg:mt-20 mt-10'>
+          <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+            <div className='md:w-16 md:h-16 w-10 h-10'>
+              <Link href='/'>
+                <>
+                  <Image
+                    width={62}
+                    height={62}
+                    className="rounded-full"
+                    src={post.postedBy.image}
+                    alt="profile pic"
+                    layout="responsive"
+                  />
+                </>
+              </Link>
+            </div>
+            <div>
+              <Link href='/'>
+                <div className='flex items-center gap-2'>
+                  <p className='flex gap-2 items-center md-text-md font-bold text-primary'>
+                    {post.postedBy.userName}{` `}
+                    <GoVerified className='text-blue-400 text-md' />
+                  </p>
+                  <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                    {post.postedBy.userName}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
