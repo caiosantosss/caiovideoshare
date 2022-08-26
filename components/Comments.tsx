f90136fc85a32a66ae908d73108b2c9d07c36e44
caiosantosss <caio.santos@msn.com>
2022-08-26 22:46:42 +0900

    add const comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -7,8 +7,18 @@ import useAuthStore from '../store/authStore';
 import NoResults from './NoResults';
 
 const Comments = () => {
+  const comments = [];
+
   return (
-    <div>Comments</div>
+    <div className='border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
+      <div className='overflow-scroll lg-h-[475px]'>
+        {comments.length ? (
+          <div>viedeos</div>
+        ) : (
+          <NoResults text='No comments yet! Be the first one to add a comment.' />
+        )}
+      </div>
+    </div>
   )
 }
 
