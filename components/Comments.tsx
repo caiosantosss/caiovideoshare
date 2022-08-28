04d69f5560f459b1c4c5ed16c8bda0380a8ef54d
caiosantosss <caio.santos@msn.com>
2022-08-28 09:27:03 +0900

    add button

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -9,6 +9,7 @@ import NoResults from './NoResults';
 const Comments = () => {
   const { userProfile } = useAuthStore();
   const comments = [];
+  const isPostingComment = false;
 
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
@@ -28,6 +29,9 @@ const Comments = () => {
               placeholder='Add a comment...'
               className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-gray-300 flex-1 rounded-lg'
             />
+            <button className='text-md text-gray-400' onClick={() =>{}}>
+              {isPostingComment ? 'Posting...' : 'Post'}
+            </button>
           </form>
         </div>
       )}
