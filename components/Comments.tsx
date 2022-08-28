135868b530ce8a576076fe90a4082cff8de7f6c8
caiosantosss <caio.santos@msn.com>
2022-08-28 09:24:48 +0900

    add input

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -7,6 +7,7 @@ import useAuthStore from '../store/authStore';
 import NoResults from './NoResults';
 
 const Comments = () => {
+  const { userProfile } = useAuthStore();
   const comments = [];
 
   return (
@@ -18,6 +19,18 @@ const Comments = () => {
           <NoResults text='No comments yet!' />
         )}
       </div>
+      {userProfile && (
+        <div className='absolute bottom-0 left-0 pb-6 px-2 md:px-10'>
+          <form onSubmit={() => {}} className="flex gap-4">
+            <input
+              value=''
+              onChange={() => {}}
+              placeholder='Add a comment...'
+              className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-gray-300 flex-1 rounded-lg'
+            />
+          </form>
+        </div>
+      )}
     </div>
   )
 }
