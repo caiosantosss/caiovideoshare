99f5b45719ae42815bd80d5ecfa3f3479c246cd5
caiosantosss <caio.santos@msn.com>
2022-08-29 20:16:41 +0900

    fix comment of null

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -24,16 +24,14 @@ interface IComment {
   };
 }
 
-const Comments = () => {
+const Comments = ({comment, setComment, addComment, comments, isPostingComment}: IProps) => {
   const { userProfile } = useAuthStore();
-  const comments = [];
-  const isPostingComment = false;
 
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
       <div className='overflow-scroll lg-h-[475px]'>
-        {comments.length ? (
-          <div>viedeos</div>
+        {comments?.length ? (
+          <div>videos</div>
         ) : (
           <NoResults text='No comments yet!' />
         )}
