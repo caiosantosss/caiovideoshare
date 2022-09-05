c18ca2b9759169cd7069a16b9e43683cd40da417
caiosantosss <caio.santos@msn.com>
2022-09-05 21:38:52 +0900

    add top part of profile

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -17,9 +17,35 @@ interface IProps {
 }
 
 const Profile = ({ data }: IProps ) => {
+  const { user, userVideos, userLikedVideos } = data;
+
+
   return (
     <div>
-      Profile
+      <div className="w-full">
+        <div className="flex gap-6 md:gap-10 mb-4 bg-white w-full">
+          <div className='w-16 h-16 md:w-32 md:h-32'>
+            <Image
+              src={user.image}
+              width={120}
+              height={120}
+              className='rounded-full'
+              alt='profile image'
+              layout='responsive'
+            />
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <p className='md:text-2xl tracking-wider flex gap-1 items-center justify-center text-md font-bold text-primary lowercase'>
+              {user.userName.replaceAll(' ', '')}
+              <GoVerified className='inline text-blue-400' />
+            </p>
+            <p className='capitalize md:text-xl text-gray-400 text-xs'>
+              {user.userName}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
