c92e835423e1457492b775f02ed45e6a0f709829
caiosantosss <caio.santos@msn.com>
2022-09-10 21:45:10 +0900

    add import and getserver to search

--- pages/search/[searchTerm].jsx
+++ pages/search/[searchTerm].jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState } from "react";
+import Image from "next/image";
+import { GoVerified } from "react-icons/go";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import VideoCard from "../../components/VideoCard";
+import NoResults from "../../components/NoResults";
+import { IUser, Video } from "../../types";
+import { BASE_URL } from "../../utils";
+import useAuthStore from "../../stores/useAuthStore";
 
 const Search = () => {
   return (
     <div>Search</div>
   )
+}
+
+export const getServerSideProps = async ({
+  params: { searchTerm },
+}) => {
+  const res = await axios.get(`${BASE_URL}/api/search/${searchTerm}`);
+
+  return {
+    props: {
+      data: res.data
+    }
+  }
 }
 
 export default Search
