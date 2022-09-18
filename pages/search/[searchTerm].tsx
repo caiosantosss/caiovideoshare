39e9549b6840524885fe3b34df77770ef3f61054
caiosantosss <caio.santos@msn.com>
2022-09-18 22:34:30 +0900

    fix searchTerm and refac

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-key */
 import React, { useState } from "react";
 import Image from "next/image";
 import { GoVerified } from "react-icons/go";
@@ -10,25 +9,26 @@ import VideoCard from "../../components/VideoCard";
 import NoResults from "../../components/NoResults";
 import { IUser, Video } from "../../types";
 import { BASE_URL } from "../../utils";
-import useAuthStore from "../../stores/useAuthStore";
+import useAuthStore from "../../store/authStore";
 
 const Search = ({ videos }: {videos: Video[]}) => {
   const [isAccounts, setIsAccounts] = useState(false);
+  const { allUsers }: { allUsers: IUser[] } = useAuthStore();
+
   const router = useRouter();
   const { searchTerm }: any = router.query;
-  const { allUsers } = useAuthStore();
 
   const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
   const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
-  const searchAccounts = allUsers.filter((user: IUser) => user.userName.toLowerCase().includes(searchTerm.toLowerCase()));
+  const searchAccounts = allUsers.filter((user: IUser) => user.userName.toLowerCase().includes(searchTerm));
 
   return (
     <div className="w-full">
       <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
-        <p className={`text-xl font-semibold cursor-pointer mt-2 ${accounts}`} onClick={() => setIsAccounts(true)}>
+        <p onClick={() => setIsAccounts(true)} className={`text-xl font-semibold cursor-pointer mt-2 ${accounts}`} >
           Accounts
         </p>
-        <p className={`text-xl font-semibold cursor-pointer mt-2 ${isVideos}`} onClick={() => setIsAccounts(false)}>
+        <p onClick={() => setIsAccounts(false)} className={`text-xl font-semibold cursor-pointer mt-2 ${isVideos}`} >
           Videos
         </p>
       </div>
@@ -36,7 +36,7 @@ const Search = ({ videos }: {videos: Video[]}) => {
         <div className="md:mt-16">
           {searchAccounts.length > 0 ? (
             searchAccounts.map((user: IUser, idx: number) => (
-              <Link href={`/profile/${user._id}`} key={idx}>
+              <Link key={idx} href={`/profile/${user._id}`} >
                 <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200'>
                   <div>
                     <Image
@@ -48,28 +48,29 @@ const Search = ({ videos }: {videos: Video[]}) => {
                       layout='responsive'
                     />
                   </div>
-
-                  <div className='hidden xl:block'>
-                    <p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
-                      {user.userName.replaceAll(' ', '')}
-                      <GoVerified className='inline text-blue-400' />
-                    </p>
-                    <p className='capitalize text-gray-400 text-xs'>
-                      {user.userName}
-                    </p>
+                  <div>
+                    <div>
+                      <p className='flex gap-1 items-center text-lg font-bold text-primary'>
+                        {user.userName}
+                        <GoVerified className='inline text-blue-400' />
+                      </p>
+                      <p className='capitalize text-gray-400 text-sm'>
+                        {user.userName}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </Link>
             ))
           ) : (
-              <NoResults text = {`No video results for ${searchTerm}`} />
+              <NoResults text = {`No account results for ${searchTerm}`} />
           )}
         </div>
       ) : (
         <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start">
           {videos.length ? (
-            videos.map((video: Video, idx ) => (
-              <VideoCard key={idx} post={video} />
+            videos.map((post: Video, idx: number ) => (
+              <VideoCard key={idx} post={post} />
             ))
           ): (
             <NoResults text={`No video results for ${searchTerm}`} />
@@ -80,13 +81,15 @@ const Search = ({ videos }: {videos: Video[]}) => {
   )
 }
 
-export const getServerSideProps = async ({ params: { searchTerm } }: { params: { searchTerm: string } }) => {
+export const getServerSideProps = async ({
+  params: { searchTerm },
+ }: {
+  params: { searchTerm: string };
+ }) => {
   const res = await axios.get(`${BASE_URL}/api/search/${searchTerm}`);
 
   return {
-    props: {
-      data: { videos: res.data}
-    }
+    props: { videos: res.data},
   }
 }
 
