d8b86c309ee98b311b31ab9f1d62b4be061fd9da
caiosantosss <caio.santos@msn.com>
2022-09-22 12:24:05 +0900

    refactor

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -19,81 +19,71 @@ interface IComment {
   comment: string;
   length?: number;
   _key: string;
-  postedBy: {
-    _ref: string;
-    _id: string;
-  };
+  postedBy: { _ref?: string; _id?: string };
 }
 
 const Comments = ({comment, setComment, addComment, comments, isPostingComment}: IProps) => {
-  const { userProfile, allUsers } = useAuthStore();
+  const { userProfile, allUsers }: any = useAuthStore();
 
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
       <div className='overflow-scroll lg-h-[475px]'>
-        {comments?.length ? (
-          comments.map((item, idx) => (
+        {comments?.length > 0 ? (
+          comments?.map((item: IComment, idx: number) => (
             <>
-              {allUsers.map((user: IUser) => (
+              {allUsers.map((user: IUser) =>
                 user._id === (item.postedBy._id || item.postedBy._ref) && (
                   <div className='p-2 items-center' key={idx}>
                     <Link href={`/profile/${user._id}`}>
                       <div className='flex items-start gap-3'>
-                        <div className='flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded'>
-                          <div className='w-8 h-8'>
-                            <Image
-                              src={user.image}
-                              width={32}
-                              height={32}
-                              className='rounded-full'
-                              alt='profile image'
-                              layout='responsive'
-                            />
-                          </div>
-
-                          <div className='hidden xl:block'>
-                            <p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
-                              {user.userName.replaceAll(' ', '')}
-                              <GoVerified className='inline text-blue-400' />
-                            </p>
-                            <p className='capitalize text-gray-400 text-xs'>
-                              {user.userName}
-                            </p>
-                          </div>
+                        <div className='w-12 h-12'>
+                          <Image
+                            src={user.image}
+                            width={48}
+                            height={48}
+                            className='rounded-full cursor-pointer'
+                            alt='profile image'
+                            layout='responsive'
+                          />
                         </div>
+
+                        <p className='flex cursor-pointer gap-1 items-center text-[18px] font-bold text-primary leading-6'>
+                          {user.userName}{' '}
+                          <GoVerified className='inline text-blue-400' />
+                        </p>
                       </div>
                     </Link>
                     <div>
-                      <p>
+                      <p className='-mt-5 ml-16 text-[16px] mr-8'>
                         {item.comment}
                       </p>
                     </div>
                   </div>
                 )
-              ))}
+              )}
             </>
           ))
         ) : (
-          <NoResults text='No comments yet!' />
+          <NoResults text='No comments yet! Be First to do add the comment.' />
         )}
       </div>
-      {userProfile && (
+      {userProfile &&
         <div className='absolute bottom-0 left-0 pb-6 px-2 md:px-10'>
           <form onSubmit={addComment} className="flex gap-4">
             <input
               value={comment}
-              onChange={(e) => setComment(e.target.value)}
+              onChange={(e) => setComment(e.target.value.trim())}
               placeholder='Add a comment...'
               className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-gray-300 flex-1 rounded-lg'
             />
-            <button className='text-md text-gray-400' onClick={() =>{}}>
+            <button className='text-md text-gray-400' onClick={addComment}>
               {isPostingComment ? 'Posting...' : 'Post'}
             </button>
           </form>
         </div>
-      )}
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
