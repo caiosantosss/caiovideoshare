aa304153985d7e5302a3ab86c93e73aa52b88843
caiosantosss <caio.santos@msn.com>
2022-09-24 20:37:19 +0900

    refactor

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { MdFavorite } from 'react-icons/md';
+import { NextPage } from 'next';
 
 import useAuthStore from '../store/authStore';
 
-interface Iprops {
+interface IProps {
   handleLike: () => void;
   handleDislike: () => void;
-  likes: any[];
+  likes: any;
+  flex: string;
 }
 
-const LikeButton = ({ likes , handleLike, handleDislike }: Iprops) => {
+const LikeButton: NextPage<IProps> = ({ likes, flex, handleLike, handleDislike }) => {
   const [alreadyLiked, setAlreadyLiked] = useState(false);
   const { userProfile }: any = useAuthStore();
-  const filterLikes = likes?.filter((item) => item._ref === userProfile?._id);
+  const filterLikes = likes?.filter((item: any) => item._ref === userProfile?._id);
 
   useEffect(() => {
-    if (filterLikes.length > 0) {
+    if (filterLikes?.length > 0) {
       setAlreadyLiked(true);
     } else {
       setAlreadyLiked(false);
@@ -24,7 +26,7 @@ const LikeButton = ({ likes , handleLike, handleDislike }: Iprops) => {
 
 
   return (
-    <div className='flex gap-6'>
+    <div className={`${flex} gap-6`}>
       <div className='mt-4 flex flex-col justify-center items-center cursor-pointer'>
         {alreadyLiked ? (
           <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997]' onClick={handleDislike}>
@@ -35,10 +37,10 @@ const LikeButton = ({ likes , handleLike, handleDislike }: Iprops) => {
             <MdFavorite className='text-lg md:text-2xl' />
           </div>
         )}
-        <p className='text-md font-semibold'>{likes?.length | 0}</p>
+        <p className='text-md font-semibold'>{likes?.length || 0}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LikeButton
+export default LikeButton;
