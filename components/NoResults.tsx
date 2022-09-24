ca04181f4eab63a247a18a1898dd0b8aa9449cbc
caiosantosss <caio.santos@msn.com>
2022-09-24 20:41:44 +0900

    refactor

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import { NextPage } from 'next';
 import { MdOutlineVideocamOff } from 'react-icons/md';
-import { BiCommentX } from 'react-icons/bi';
 
 interface IProps {
   text: string;
 }
 
-const NoResults: NextPage<IProps> = ({ text }) => {
+const NoResults = ({ text }: IProps) => {
   return (
     <div className='flex flex-col justify-center items-center h-full w-full'>
       <p className='text-8xl'>
-        {text === 'No comments yet!'
-          ? <BiCommentX />
-          : <MdOutlineVideocamOff />
-        }
+        <MdOutlineVideocamOff />
       </p>
       <p className='text-2xl text-center'>
         {text}
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default NoResults
+export default NoResults;
